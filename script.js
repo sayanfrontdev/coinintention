cd98244b8e51be1d119e1d89a150de2ff039c97e
Sayan <sayanmanda1922001@gmail.com>
2024-02-19 04:17:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width= "250px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width= 0;
+
+    
+}
